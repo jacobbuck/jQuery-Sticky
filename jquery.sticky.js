b86c474b319ea8da37de2bb02906ae0509d2a40b
Jacob Buck <buck.jacob@gmail.com>
2015-02-07 12:10:32 +1300

    Major refactor. Build sticky more like my cling plugin. Add the ability to destroy. Add DOM throttling for better performance. Add better comments. Update copyright.

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,129 +1,200 @@
 /*!
- * jQuery Sticky - Copyright (c) 2014 Jacob Buck
- * https://github.com/jacobbuck/jQuery-Sticky
+ * Sticky - Copyright (c) 2015 Jacob Buck
+ * https://github.com/jacobbuck/sticky
  * Licensed under the terms of the MIT license.
  */
-
 (function (factory) {
 	// UMD yo!
 	if (typeof define === 'function' && define.amd) {
 		define(['jquery'], factory);
 	} else {
-		factory(jQuery);
+		factory(window.jQuery);
 	}
 }(function ($) {
+	'use strict';
 
-	// Cache the window, hack the planet!
-	var $window  = $(window);
+	// Constants
 
-	// Hey, it's our plugin.
-	$.fn.sticky = function () {
+	var STUCK_CLASSNAME = 'stuck';
 
-		return this.each(function () {
+	// Utilities
 
-			// Cache our element and it's offset parent.
-			var $element      = $(this);
-			var $offsetParent = $element.offsetParent();
+	function toNumber (num) {
+		return isFinite(num = parseFloat(num)) ? num : 0;
+	}
 
-			// Wrapper for our element (acts as a placeholder).
-			var $elementWrap  = $('<div>').insertBefore($element).append($element);
+	function throttle (fn, context) {
+		var timer = false;
+		return function () {
+			var c = context || this;
+			var a = arguments;
+			if (false === timer) {
+				timer = window.requestAnimationFrame(function () {
+					fn.apply(c, a);
+					timer = false;
+				});
+			}
+		};
+	}
 
-			// Store the elements inherit position style
-			var elementPosition;
+	// Sticky
 
-			// Where the action-magic happpens!
-			function update () {
+	function Sticky (element) {
+		// Cache our element and it's offset parent.
+		this.e = element;
+		this.o = element.offsetParent();
 
-				// Check if our element is already stuck
-				var stuck = $element.hasClass('stuck');
+		// Wrapper for our element (acts as a placeholder).
+		this.w = $('<div>').insertBefore(element).append(element);
 
-				// Using getBoundingClientRect as jQuery's offset/position isn't good enough.
-				var elementWrapBCR = $elementWrap.get(0).getBoundingClientRect();
-				var offsetParentBCR = $offsetParent.get(0).getBoundingClientRect();
+		// Store the elements inherit position style
+		this.p = element.css('position');
 
-				// Get some element stlyes
-				var elementMarginTop = parseInt($element.css('margin-top'));
-				var elementMarginBottom = parseInt($element.css('margin-bottom'));
-				var elementOuterHeight = $element.outerHeight(true);
-				var elementOuterWidth = $element.outerWidth(true);
+		// Throttle update method
+		this.update = throttle(this.update, this);
 
-				if (!stuck) {
-					elementPosition = $element.css('position');
+		// Bind some events.
+		$(window).on('load resize scroll touchmove', this.update);
+
+		this.update();
+	}
+
+	Sticky.prototype = {
+		stuck: false,
+
+		update: function () {
+			var element = this.e;
+			var oparent = this.o;
+			var wrapper = this.w;
+
+			// Using getBoundingClientRect as jQuery's offset/position isn't good enough.
+			var wrapperBCR = wrapper.get(0).getBoundingClientRect();
+			var oparentBCR = oparent.get(0).getBoundingClientRect();
+
+			// Get some element styles
+			var elementMarginTop = toNumber(element.css('margin-top'));
+			var elementMarginBottom = toNumber(element.css('margin-bottom'));
+
+			// Update the elements inherit position style
+			if (!this.stuck) {
+				this.p = element.css('position');
+			}
+
+			// Check if element should be stuck.
+			// (1) The element's wrapper is beyond the top of the screen
+			// (2) The element's offset parent within view
+			// (3) The element is visible
+			if (
+				wrapperBCR.top + elementMarginTop < 0 && // (1)
+				(oparentBCR.top + elementMarginTop > 0 ||
+					oparentBCR.bottom + elementMarginBottom > 0) && // (2)
+				element.is(':visible') // (3)
+			) {
+				// Get some more element styles
+				var elementOuterHeight = element.outerHeight(true);
+				var elementOuterWidth = element.outerWidth(true);
+
+				// Cache new CSS values and set CSS once we're finished, to prevent DOM thrashing.
+				var elementCSS = {};
+				var wrapperCSS = {};
+
+				// Stick our element if it isn't already
+				if (!this.stuck) {
+					this.stuck = true;
+					element.addClass(STUCK_CLASSNAME).trigger('stuck');
+					elementCSS.position = 'fixed';
 				}
 
-				// Cache new SS values and set CSS once we're finished, to prevent DOM thrashing.
-				var elementCSS = {};
-				var elementWrapCSS = {};
+				// Keep our element inside its offset parent
+				elementCSS.top = Math.min(-elementMarginTop, oparentBCR.bottom - elementOuterHeight);
 
-				// Check if element should be stuck.
-				// (1) The elements wrapper is beyond the top of the screen
-				// (2) The elements offset parent within view
-				// (3) The element is visible
-				if (
-					elementWrapBCR.top + elementMarginTop < 0 && // (1)
-					(offsetParentBCR.top + elementMarginTop > 0 ||
-						offsetParentBCR.bottom + elementMarginBottom > 0) && // (2)
-					$element.is(':visible') // (3)
+				// And align it with its wrapper
+				elementCSS.left = wrapperBCR.left;
+
+				// Check if our element has changed size...
+				if (!this.stuck ||
+					elementOuterWidth != wrapper.width() ||
+					elementOuterHeight != wrapper.height()
 				) {
-
-					// Stick our element if it isn't already
-					if (!stuck) {
-						$element.addClass('stuck').trigger('stuck');
-						elementCSS.position = 'fixed';
+					// ... and keep the dimentions of the element and its wrapper n*sync.
+					elementCSS.width = wrapper.width();
+					if (!/absolute|fixed/.test(this.p)) {
+						wrapperCSS.height = elementOuterHeight;
 					}
-
-					// Keep our element inside it's offset parent
-					elementCSS.top = Math.min(-elementMarginTop, offsetParentBCR.bottom - elementOuterHeight);
-
-					// And align it with it's wrapper
-					elementCSS.left = elementWrapBCR.left;
-
-					// Check if our element has changed size...
-					if (!stuck ||
-						elementOuterWidth != $elementWrap.width() ||
-						elementOuterHeight != $elementWrap.height()
-					) {
-						// ... and keep the dimentions of the element and it's wrapper n*sync.
-						elementCSS.width = elementOuterWidth;
-						if (!/absolute|fixed/.test(elementPosition)) {
-							elementWrapCSS.height = elementOuterHeight;
-						}
-					}
-
-				} else if (stuck) {
-
-					// Reset everything if our element is stuck and shouldn't be anymore.
-					elementCSS = {
-						width: '',
-						margin: '',
-						position: '',
-						top: '',
-						left: ''
-					};
-					elementWrapCSS = {
-						height: '',
-						margin: ''
-					};
-					$element.removeClass('stuck').trigger('unstuck');
-
 				}
 
 				// Set new CSS, as mentioned above.
-				$element.css(elementCSS);
-				$elementWrap.css(elementWrapCSS);
+				element.css(elementCSS);
+				wrapper.css(wrapperCSS);
 
-				// That's all folks!
+			} else if (this.stuck) {
+				// Reset everything if our element is stuck and shouldn't be anymore.
+				this.reset();
 			}
 
-			// Bind some events.
-			$window.on('load resize scroll touchmove', update);
+			// That's all folks!
+		},
 
-			// Aaaannnnd... go!
-			update();
-		});
+		reset: function () {
+			var element = this.e;
+			var wrapper = this.w;
 
+			this.stuck = false;
+
+			wrapper.css({
+				height: '',
+				margin: ''
+			});
+
+			element.css({
+				width: '',
+				margin: '',
+				position: '',
+				top: '',
+				left: ''
+			})
+			.removeClass(STUCK_CLASSNAME)
+			.trigger('unstuck');
+		},
+
+		destroy: function () {
+			var element = this.e;
+			var wrapper = this.w;
+
+			// Reset everything
+			this.reset();
+
+			// Remove element wrapper
+			wrapper.replaceWith(element);
+
+			// Unbind events
+			$(window).off('load resize scroll touchmove', this.update);
+		}
 	};
 
-	// Thanks for reading :)
+	// Hey, it's our jQuery plugin.
+	$.fn.extend({
+		sticky: function (target, options) {
+			target = $(target);
+
+			return this.each(function () {
+				var element = $(this);
+				var instance = element.data('sticky');
+
+				if (instance instanceof Sticky) {
+					if ($.isFunction(instance[target])) {
+						instance[target](options);
+
+						// Remove stored instance
+						if ('destroy' == target) {
+							element.removeData('sticky');
+						}
+					}
+				} else {
+					element.data('sticky', new Sticky(element));
+				}
+			});
+		}
+	});
 
 }));
